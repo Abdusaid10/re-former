92501b19e13d5c5c9a5589a7467e1a7b56a2fe10
Abdusaid10 <Abdusaid10@github.com>
2019-08-26 18:29:35 +0500

    edit action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,28 @@
 class UsersController < ApplicationController
     def new 
-        @user = User.new
     end
     def create
-      @user = User.new(whitelisted_post_params)
-    #   @user=User.new(username: params[:username], email: params[:email], password: params[:password])  
+    #   @user = User.new(whitelisted_post_params)
+      @user=User.new(username: params[:username], email: params[:email], password: params[:password])  
       if @user.save
         redirect_to new_user_path
       else
         render :new
       end
-
     end
+
+    def edit
+        @user=User.find(params[:id])
+    end
+
+    def update
+        @user=User.find(params[:id])
+        @user.update(username:  params[:username], email: params[:email], password: params[:password])
+    end
+
     def show
     end
+
     private
     def whitelisted_post_params
         params.require(:user).permit(:username,:email,:password)
