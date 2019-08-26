cb4b60cb8463d388347bb2df7e5ab0757eea7ebb
imhta <imhtapm@gmail.com>
2019-08-26 18:31:07 +0530

    Rebuild the form using #form_for and user controlller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,14 @@ class UsersController < ApplicationController
         @user = User.new
     end
     def create
-    #   @user = User.new(whitelisted_post_params)
-      @user=User.new(username: params[:username], email: params[:email], password: params[:password])  
+      @user = User.new(whitelisted_post_params)
+    #   @user=User.new(username: params[:username], email: params[:email], password: params[:password])  
       if @user.save
         redirect_to new_user_path
       else
         render :new
       end
+
     end
     def show
     end
