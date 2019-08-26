9bc578ec4defcaa0465cf3645711581350b3acb1
imhta <imhtapm@gmail.com>
2019-08-26 17:10:05 +0530

    added whitelist params to user new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
     def new 
     end
     def create
-      @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+      @user = User.new(whitelisted_post_params)
 
       if @user.save
         redirect_to new_user_path
@@ -11,5 +11,9 @@ class UsersController < ApplicationController
       end
     end
     def show
+    end
+    private
+    def whitelisted_post_params
+        params.require(:user).permit(:username,:email,:password)
     end
 end
